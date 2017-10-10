8fccc3263045922d909296f783ad1d3ddb8ab85a
Michael Tran <michaelhuytran@gmail.com>
2017-10-10 16:36:04 -0700

    .

--- pin_clone.py
+++ pin_clone.py
@@ -1,19 +1,31 @@
 from flask import Flask
+from flask_restless import APIManager
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, Text
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pin.db'
 db = SQLAlchemy(app)
+
 
 class Pin(db.Model):
     id = Column(Integer, primary_key=True)
     title = Column(Text, unique=False)
     image = Column(Text, unique=False)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
+db.create_all()
+
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(Pin, methods=['GET', 'POST', 'DELETE', 'PUT'])
+
+
+@app.route('/index')
+def index():
+    return app.send_static_file('index.html')
+
+
+app.debug = True
 
 if __name__ == '__main__':
     app.run()
