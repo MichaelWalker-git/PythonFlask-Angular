567605a2395afb7e27302c910cb4af5e2b9fc59a
Michael Tran <michaelhuytran@gmail.com>
2017-10-10 17:21:55 -0700

    .

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,17 @@
 const app =  angular.module("app", []);
 
-app.controller("AppCtrl", function () {
+app.controller("AppCtrl", function ($http) {
     const app = this;
-    app.message = "Am I working?";
 
-    $http.get("/api/pin").success((function (data) {
-        app.pins = data.objects;
-    }))
+    $http.get("/api/pin").then(function (data) {
+        app.pins = data.data.objects;
+        console.log(data)
+    });
+
+    app.addPin = function () {
+        $http.post("api/pin", {"title": "new", "image": "http:///placekitten.com/200/200/?image=" + app.pins.length })
+            .then(function (response) {
+                app.pins.push(response);
+            })
+    }
 });
